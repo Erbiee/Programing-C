2a8f1549d6f76ecc00240ac5f199367812a1ccfb
Erbiee <nsigarev6@gmail.com>
2024-10-05 01:35:51 +0700

    5.c

--- lab_4/5.c
+++ lab_4/5.c
@@ -19,16 +19,16 @@ int main() {
 		}
 		printf("\n");
 	}
-	for (int i = 0; i < mid - 1; i++) {
-		for (int j = 0; j < (h - 1) / 2 - 1 - i; j++) {
+	for (int i = mid - 2; i >= 0; i--) {
+		for (int j = 0; j < i; j++) {
 			printf(" ");
 		}
 
-		cnt_sym = 3 + 2 * i;
-		for (int j = mid - 2 - i; j < mid; j++) {
+		cnt_sym = h - i * 2;
+		for (int j = i; j < mid; j++) {
 			printf("%c", 65 + j);
 		}
-		for (int j = mid - 2, k = 0; k < (cnt_sym - 1) / 2; j--, k++) {
+		for (int j = mid - 2; j >= i; j--) {
 			printf("%c", 65 + j);
 		}
 		printf("\n");
